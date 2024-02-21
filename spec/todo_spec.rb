331f89e738681413198a506b3f245a523d961708
Will Browning <wdbrwnng@memphis.edu>
2024-02-21 17:25:40 -0600

    Problem 2: Todo List

---
+++ spec/todo_spec.rb
@@ -0,0 +1,23 @@
+require 'todo'
+
+RSpec.describe TodoList do
+  let(:todo1){TodoList.new}
+  describe ".add" do
+    it "adds a todo to the list" do
+      list_1 = @todos
+      expect(todo1.add('Make mac and cheese')).to eq(['Make mac and cheese'])
+    end
+  end
+
+  describe ".remove" do
+    it "removes a todo from the list" do
+      expect(todo1.remove('Make mac and cheese')) unless todo1.todos.include?('Make mac and chees')
+    end
+  end
+
+  describe "#todos" do
+    it "returns all todos" do
+      expect(todo1.todos).to eq(todo1.todos)
+    end
+  end
+end
